bdc0bdf37b2bbf1e63b0590a0f603aa0fdd39438
DOHYUN <pastapeter@naver.com>
2021-03-14 17:19:43 +0900

    10870

--- 10870/10870/main.swift
+++ 10870/10870/main.swift
@@ -7,5 +7,18 @@
 
 import Foundation
 
-print("Hello, World!")
+func fibonazzi(n: Int) -> Int {
+    if n == 0 {
+        return 0
+    } else if n == 1 {
+        return 1
+    } else {
+        return fibonazzi(n: n-1) + fibonazzi(n: n-2)
+    }
+}
+
+var input = readLine()
+if let input = input {
+    print(fibonazzi(n: Int(input)!))
+}
 
